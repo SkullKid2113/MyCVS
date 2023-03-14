8ff9c85914953ca74ecdee2b960dcfb11794cea7
SkullKid2113 <125101202+SkullKid2113@users.noreply.github.com>
2023-03-14 18:37:25 -0500

    #1 Jasmine's booty fix

--- spec/it-works.spec.js
+++ spec/it-works.spec.js
@@ -2,4 +2,4 @@ describe("csv-parse", function() {
     it("reads and writes file in a csv format", function() {
         expect(false).toBe(true);
     });
-        console.log(data);
+        console.log(data); })
